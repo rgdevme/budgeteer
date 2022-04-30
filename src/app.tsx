257f1a569e0c421ff26811731a8ced6cadd7b8a5
Rafael Gonzalez <rafael.gonzalez81@gmail.com>
2022-04-30 13:31:08 -0400

    chore: initial commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import Home from './screens/home'
+import Select from './screens/select'
+
+export function App() {
+	return (
+		<StrictMode>
+			<Home />
+		</StrictMode>
+	)
+}
